dabc0b212d74b74fc9c8c07c3df91d7b99e59c6f
Alv-P <alvinpaulp08@gmail.com>
2025-07-18 13:06:24 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen p-8 bg-[color:var(--background)] text-[color:var(--foreground)] font-sans">
